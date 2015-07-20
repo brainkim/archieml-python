febc1746d0897aa477584c531bdc56bff14e99b8
Brian Kim <briankimpossible@gmail.com>
2015-07-20 00:46:58 -0400

    Make tests suck less

--- tests/test_archieml.py
+++ tests/test_archieml.py
@@ -6,23 +6,27 @@ import unittest
 import archieml
 
 class TestArchieML(unittest.TestCase):
-    def test_loader(self):
-        testdir = os.path.dirname(os.path.realpath(__file__))
-        for filename in glob.glob('{}/archieml.org/test/1.0/*.aml'.format(testdir)):
-            if 'all.0.aml' not in filename and 'freeform' not in filename:
-                slug, i = os.path.basename(filename).split('.')[:2]
-                with open(filename) as f:
-                    metadata = archieml.load(f)
-                    test = metadata['test']
-                    expected = json.loads(metadata['result'])
+    def check_file(self, filename):
+        with open(filename) as f:
+            metadata = archieml.load(f)
+            test = metadata['test']
+            expected = json.loads(metadata['result'])
 
-                with open(filename) as f:
-                    actual = archieml.load(f)
-                    del actual['test']
-                    del actual['result']
+        with open(filename) as f:
+            actual = archieml.load(f)
+            del actual['test']
+            del actual['result']
 
-                self.assertEqual(expected, actual, (
-                    "{slug}.{i} {test}\n"
-                    "expected: {expected}\n"
-                    "actual  : {actual}"
-                ).format(slug=slug, i=i, test=test, expected=expected, actual=actual))
+        self.assertEqual(expected, actual, (
+            '{}\n'
+            'expected: {}\n'
+            'actual  : {}'
+        ).format(test, expected, actual))
+
+testdir = os.path.dirname(os.path.realpath(__file__))
+files = glob.glob('{}/archieml.org/test/1.0/*.aml'.format(testdir))
+files = filter(lambda f: 'all.0.aml' not in f and 'freeform' not in f, files)
+
+for filename in files:
+    slug, i = os.path.basename(filename).split('.')[:2]
+    setattr(TestArchieML, 'test_{}_{}'.format(slug, i), lambda self: self.check_file(filename))
